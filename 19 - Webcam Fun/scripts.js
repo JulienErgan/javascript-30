4a4251217eeeed0c0537c292c42aa7453ae3a353
Julien Ergan <julien.ergan.code@gmail.com>
2020-07-14 16:13:55 +0900

    functions red Effect and rbgSplit

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -35,6 +35,18 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // console.log(pixels);
+    // debugger;
+
+    // mess with the pixels
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
+
+    // put the pixels back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -54,6 +66,24 @@ function takePhoto() {
   // link.textContent = 'Download Image'; // instead of this:
   link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) { // pixels.data is the array
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red from rgba
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) { // pixels.data is the array
+    pixels.data[i -150] = pixels.data[i + 0]; // red from rgba
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
 }
 
 getVideo();
