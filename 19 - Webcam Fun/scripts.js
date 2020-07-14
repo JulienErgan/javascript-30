50cb4f18d422e0f7dff0845416d309fdeef9f63c
Julien Ergan <julien.ergan.code@gmail.com>
2020-07-14 15:52:55 +0900

    functions paintToCanvas and takePhoto

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -17,11 +17,46 @@ function getVideo() {
 
       video.srcObject = localMediaStream;
       video.play();
-    });
+    })
     // to handle error if video is not allowed
     .catch(err => {
-      console.error('OH NOOO!', err);
+      console.error(`OH NOOO!`, err);
     });
 }
 
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  // console.log(width, height); // size of the video coming out from the webcam
+  // but need to make sure that the canvas is the same size before we paint to it
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  console.log(data); // if take a photo in the browser, in the console it returns
+  // something called Base64 which is a text-based representation of the photo
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  // link.textContent = 'Download Image'; // instead of this:
+  link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas);
